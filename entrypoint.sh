6fab5ba711cae6c129a07cfdec5fda680cdf1864
AVAROZ <tomasfjm@hotmail.com>
2026-08-20 23:59:35 -0500

    Add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Starting Tailscale daemon..."
+tailscaled --tun=userspace-networking &
+TAILSCALE_PID=$!
+sleep 3
+
+if [ -n "$TAILSCALE_AUTHKEY" ]; then
+  echo "Connecting to Tailscale..."
+  tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=palworld-server --accept-dns=false
+  echo "Tailscale connected successfully"
+else
+  echo "Warning: TAILSCALE_AUTHKEY not set. Tailscale will not connect."
+fi
+
+echo "Starting Palworld server..."
+exec /init
+
